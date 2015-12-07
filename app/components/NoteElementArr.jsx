7f3f53f85155a2cb3114fcbb58126da386c59be2
wei.he <9638527_4_1@163.com>
2015-12-07 20:22:56 +0800

    edit Note Element Arr key property

--- app/components/NoteElementArr.jsx
+++ app/components/NoteElementArr.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-let elements = [<h1>Hello world</h1>, <h2>React is awesome</h2>];
+let elements = [<h1 key="1">Hello world</h1>, <h2 key="2">React is awesome</h2>];
 
 export default class NoteElementArr extends React.Component {
 	render() {
